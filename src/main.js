504e287ca39d91779e675f31a6e9a96371923020
gowthampandiyank <gowthampandiyan7@gmail.com>
2024-04-17 12:40:42 +0530

    filis added

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import '../style/style.css';
+import '../style/modern_normalized.css';
+import '../style/components/header.css';
+import '../style/components/hero.css';
+import '../style/components/about.css';
+import '../style/components/work.css';
+import '../style/components/contact.css';
+import '../style/components/footer.css';
+import '../style/components/mobilenav.css';
+import '../style/unitiy.css';
+
+import mobileNav from './utils/mobile-nav';
+import darkMode from './utils/dark-mode';
+mobileNav();
+darkMode();
